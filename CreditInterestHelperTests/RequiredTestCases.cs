a6a5b009fb5442dda88893e1dc836570b2b95182
Alex <alwill@siue.edu>
2018-02-02 21:18:07 -0600

    added first required test case

--- CreditInterestHelperTests/RequiredTestCases.cs
+++ CreditInterestHelperTests/RequiredTestCases.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using CreditInterestHelper;
+using Xunit;
+
 namespace CreditInterestHelperTests
 {
     public class RequiredTestCases
     {
-        public RequiredTestCases()
+        [Fact]
+        public void RetrieveInterestForWallets_OnePersonOneWalletThreeCards_ReturnsInterest()
         {
+            ICreditCard masterCard = new MasterCard(100);
+            ICreditCard discoverCard = new Discover(100);
+            ICreditCard visaCard = new Visa(100);
+            IWallet wallet = new Wallet(discoverCard, masterCard, visaCard);
+            IPerson person = new Person(wallet);
+
+            decimal totalInterest = person.RetrieveInterestForWallets();
+
+            Assert.Equal(16, totalInterest);
         }
     }
 }
