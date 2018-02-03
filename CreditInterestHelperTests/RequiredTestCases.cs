d9c91d5ead4eda26c31a1bcbd2d33e693b977f89
Alex <alwill@siue.edu>
2018-02-02 21:43:41 -0600

    added other cases

--- CreditInterestHelperTests/RequiredTestCases.cs
+++ CreditInterestHelperTests/RequiredTestCases.cs
@@ -15,9 +15,43 @@ namespace CreditInterestHelperTests
             IWallet wallet = new Wallet(discoverCard, masterCard, visaCard);
             IPerson person = new Person(wallet);
 
-            decimal totalInterest = person.RetrieveInterestForWallets();
+            decimal totalInterestForPerson = person.RetrieveInterestForWallets();
 
-            Assert.Equal(16, totalInterest);
+            Assert.Equal(16, totalInterestForPerson);
+        }
+
+        [Fact]
+        public void RetrieveInterestForWallets_OnePersonTwoWalletsThreeCards_ReturnsInterest()
+        {
+            ICreditCard masterCard = new MasterCard(100);
+            ICreditCard discoverCard = new Discover(100);
+            ICreditCard visaCard = new Visa(100);
+            IWallet wallet = new Wallet(discoverCard, visaCard);
+            IWallet wallet2 = new Wallet(masterCard);
+            IPerson person = new Person(wallet, wallet2);
+
+            decimal totalInterestForPerson = person.RetrieveInterestForWallets();
+
+            Assert.Equal(16, totalInterestForPerson);
+        }
+
+        [Fact]
+        public void RetrieveInterestForWallets_TwoPersonsTwoWalletsFiveCards_ReturnsInterest()
+        {
+            ICreditCard masterCard = new MasterCard(100);
+            ICreditCard discoverCard = new Discover(100);
+            ICreditCard visaCard = new Visa(100);
+            ICreditCard masterCard2 = new MasterCard(100);
+            ICreditCard visaCard2 = new Visa(100);
+            IWallet wallet = new Wallet(discoverCard, visaCard, masterCard);
+            IWallet wallet2 = new Wallet(masterCard, visaCard);
+            IPerson person = new Person(wallet);
+            IPerson person2 = new Person(wallet2);
+
+            decimal totalInterestForPersons = person.RetrieveInterestForWallets();
+            totalInterestForPersons += person2.RetrieveInterestForWallets();
+
+            Assert.Equal(31, totalInterestForPersons);
         }
     }
 }
