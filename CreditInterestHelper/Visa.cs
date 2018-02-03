ab160c5748eb3ed7b82fa7c7124fdc74664b6cc0
Alex <alwill@siue.edu>
2018-02-02 21:09:45 -0600

    added visa

---
+++ CreditInterestHelper/Visa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CreditInterestHelper
+{
+    public class Visa : ICreditCard
+    {
+        public decimal InterestRate { get; set; } = 0.1m;
+        public decimal Balance { get; set; }
+
+        public Visa(decimal amount)
+        {
+            Balance = amount;
+        }
+
+        public Visa(decimal interestRate, decimal amount)
+        {
+            Balance = amount;
+            InterestRate = interestRate;
+        }
+
+        public decimal CalculateInterest()
+        {
+            if (Balance <= 0)
+            {
+                return 0;
+            }
+            return Balance * InterestRate;
+        }
+    }
+    }
+}
